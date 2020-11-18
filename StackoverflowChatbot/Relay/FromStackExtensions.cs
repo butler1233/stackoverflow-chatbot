7722e75a6b2892bbe31f8e3628d0295dcd2a0b5f
Lee Butler <github@leerb.co.uk>
2020-11-18 15:35:41 +0000

    pls fixed

--- StackoverflowChatbot/Relay/FromStackExtensions.cs
+++ StackoverflowChatbot/Relay/FromStackExtensions.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using Discord;
+using RestSharp.Extensions;
 
 namespace StackoverflowChatbot.Relay
 {
 	internal static class FromStackExtensions
 	{
-		private static readonly Regex obImageMatch = new Regex("<img src=\"(.+)(?= \" class)");
+		private static readonly Regex obImageMatch = new Regex("<img src=\\\"([^\"]+)\" class=\"user-image");
 		private static readonly Regex obXkcdMatch = new Regex("<a rel=\"[^\"]+\" href=\"([^\"]+)\"><img src=\"([^\"]+)\" title=\"([^\"]+)\" alt=");
 		internal static DiscordMessageModel OneboxImages(this DiscordMessageModel model)
 		{
 			//Try normla images
 			var embed = new EmbedBuilder();
+			
 			var match = obImageMatch.Match(model.MessageContent);
 			if (match.Success)
 			{
 
 				var imguri = match.Groups[1].Value;
 				if (imguri.StartsWith("//")) imguri = $"https:" + imguri;
-				embed.ImageUrl = imguri;
+				embed.WithImageUrl(imguri);
 
 				model.Embed = embed.Build();
 				model.MessageContent = "";
@@ -31,10 +33,12 @@ namespace StackoverflowChatbot.Relay
 			var xkcd = obXkcdMatch.Match(model.MessageContent);
 			if (xkcd.Success)
 			{
-				embed.ImageUrl = match.Groups[2].Value;
-				embed.Footer = new EmbedFooterBuilder();
-				embed.Footer.Text = match.Groups[3].Value;
-				embed.Url = match.Groups[1].Value;
+
+				
+				embed.WithFooter(match.Groups[3].Value);
+				embed.WithUrl(match.Groups[1].Value);
+				embed.WithImageUrl(match.Groups[2].Value);
+				
 				model.MessageContent = "";
 				model.Embed = embed.Build();
 				return model;
