3276c68e0296e0c8d1011c1a35bb2d1774c88875
Deathspike <>
2020-11-19 12:25:52 +0100

    Rewrite SO HTML processing for readability and profit

--- StackoverflowChatbot/Relay/FromStackExtensions.cs
+++ StackoverflowChatbot/Relay/FromStackExtensions.cs
@@ -1,53 +1,66 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
-using Discord;
-using RestSharp.Extensions;
+using System.IO;
+using System.Linq;
+using HtmlAgilityPack;
 
 namespace StackoverflowChatbot.Relay
 {
 	internal static class FromStackExtensions
 	{
-		private static readonly Regex obImageMatch = new Regex("<img src=\\\"([^\"]+)\" class=\"user-image");
-		private static readonly Regex obXkcdMatch = new Regex("<a rel=\"[^\"]+\" href=\"([^\"]+)\"><img src=\"([^\"]+)\" title=\"([^\"]+)\" alt=");
-		internal static DiscordMessageModel OneboxImages(this DiscordMessageModel model)
+		internal static string ProcessStackMessage(this string message, int roomId, string roomName)
 		{
-			//Try normla images
-			var embed = new EmbedBuilder();
-			
-			var match = obImageMatch.Match(model.MessageContent);
-			if (match.Success)
+			var baseUri = new Uri($"https://chat.stackoverflow.com/rooms/{roomId}/{roomName}");
+			var document = new HtmlDocument();
+			document.LoadHtml(message);
+			var rootNode = document.DocumentNode.FirstChild;
+
+			// Handle code-blocks.
+			if (rootNode.Name.Equals("pre"))
 			{
-
-				var imguri = match.Groups[1].Value;
-				if (imguri.StartsWith("//")) imguri = $"https:" + imguri;
-				embed.WithImageUrl(imguri);
-
-				model.Embed = embed.Build();
-				model.MessageContent = "";
-				return model;
+				var text = HtmlEntity.DeEntitize(document.DocumentNode.InnerText);
+				return $"```\r\n{text}\r\n```";
 			}
 
-			//Try XKCD
-			var xkcd = obXkcdMatch.Match(model.MessageContent);
-			if (xkcd.Success)
+			// Handle onebox-content.
+			var classes = rootNode.GetAttributeValue("class", string.Empty).Split(' ').Select(x => x.ToLowerInvariant()).ToHashSet();
+			if (classes.Contains("ob-xkcd") || classes.Contains("ob-image"))
 			{
-
-				
-				embed.WithFooter(match.Groups[3].Value);
-				embed.WithUrl(match.Groups[1].Value);
-				embed.WithImageUrl(match.Groups[2].Value);
-				
-				model.MessageContent = "";
-				model.Embed = embed.Build();
-				return model;
+				var img = document.DocumentNode.SelectSingleNode("//img");
+				var src = HtmlEntity.DeEntitize(img?.GetAttributeValue("src", string.Empty));
+				if (Uri.TryCreate(baseUri, src, out var result)) return result.ToString();
+			}
+			else if (classes.Contains("ob-youtube") || classes.Contains("ob-message"))
+			{
+				var anchor = document.DocumentNode.SelectSingleNode("//a");
+				var href = HtmlEntity.DeEntitize(anchor?.GetAttributeValue("href", string.Empty));
+				if (Uri.TryCreate(baseUri, href, out var result)) return result.ToString();
 			}
 
-			//No matches, return as is.
-
-			return model;
+			// Handle (multiline) text.
+			using (var writer = new StringWriter())
+			{
+				ConvertTo(rootNode, writer);
+				writer.Flush();
+				return writer.ToString();
+			}
 		}
 
+		private static void ConvertTo(HtmlNode node, TextWriter writer)
+		{
+			switch (node.NodeType)
+			{
+				case HtmlNodeType.Element:
+					if (node.Name.Equals("p") || node.Name.Equals("br"))
+						writer.Write("\r\n");
+					if (node.HasChildNodes)
+						foreach (var childNode in node.ChildNodes)
+							ConvertTo(childNode, writer);
+					break;
+				case HtmlNodeType.Text:
+					if (node.InnerText.Trim().Length == 0) break;
+					writer.Write(HtmlEntity.DeEntitize(node.InnerText.Trim()));
+					break;
+			}
+		}
 	}
 }
