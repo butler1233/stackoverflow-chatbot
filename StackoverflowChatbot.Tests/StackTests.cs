2f2ccf223af52cadd3a880665e537b7241c5de68
Márton Garai <garaimarton.mail@gmail.com>
2020-11-19 16:28:01 +0100

    Convert html parse tests to NUnit + Fluent.

--- StackoverflowChatbot.Tests/StackTests.cs
+++ StackoverflowChatbot.Tests/StackTests.cs
@@ -1,76 +1,23 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StackoverflowChatbot.Relay;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace StackoverflowChatbot.Tests
 {
-	[TestClass]
+	[TestFixture]
 	public class StackTests
 	{
 		private const int RoomId = 7;
 		private const string RoomName = "c";
 
-		[TestMethod]
-		public void MessageWithMultipleBlocks()
-		{
-			var html = "Hello <b>world</b>, how are <i>you?</i>";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "Hello world, how are you?");
-		}
-
-		[TestMethod]
-		public void MessageHtml()
-		{
-			var html = "Foo &#xA9; bar";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "Foo © bar");
-		}
-
-		[TestMethod]
-		public void MessageMultiline()
-		{
-			var html = "<div class=\"full\">a <br> a</div>";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "a \r\na");
-		}
-
-		[TestMethod]
-		public void OneBoxCode()
-		{
-			var html = "<pre class=\"full\">@{\r\n    string email = item.Email;\r\n    if(item.isAdmin) email = $\"&lt;strong&gt;{email}&lt;/strong&gt;\r\n}\r\n@email</pre>";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "```\r\n@{\r\n    string email = item.Email;\r\n    if(item.isAdmin) email = $\"<strong>{email}</strong>\r\n}\r\n@email\r\n```");
-		}
-
-		[TestMethod]
-		public void OneboxMessage()
-		{
-			var html = "<div class=\"onebox ob-message\"><a rel=\"noopener noreferrer\" class=\"roomname\" href=\"/transcript/message/50954410#50954410\"><span title=\"2020-11-18 09:25:12Z\">46 mins ago</span></a>, by <span class=\"user-name\">d4rk4ng31</span> <br><div class=\"quote\">@Botler SHUT THE FUCK UP!</div></div>";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "https://chat.stackoverflow.com/transcript/message/50954410#50954410");
-		}
-
-		[TestMethod]
-		public void OneboxImage()
-		{
-			var html = "<div class=\"onebox ob-image\"><a rel=\"nofollow noopener noreferrer\" href=\"https://i.redd.it/7apl34bga1061.png\"><img src=\"https://i.redd.it/7apl34bga1061.png\" class=\"user-image\" alt=\"user image\"></a></div>";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "https://i.redd.it/7apl34bga1061.png");
-		}
-
-		[TestMethod]
-		public void OneboxXkcd()
-		{
-			var html = "<div class=\"onebox ob-xkcd\"><a rel=\"nofollow noopener noreferrer\" href=\"https://xkcd.com/2387\"><img src=\"https://imgs.xkcd.com/comics/blair_witch.png\" title=\"&quot;Are you concerned the witches won't breed in captivity?&quot; &quot;Honestly, we're more concerned that they WILL. We don't know what it involves, but our biologists theorize that it's 'harrowing.'&quot;\" alt=\"&quot;Are you concerned the witches won't breed in captivity?&quot; &quot;Honestly, we're more concerned that they WILL. We don't know what it involves, but our biologists theorize that it's 'harrowing.'&quot;\"></a></div>";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "https://imgs.xkcd.com/comics/blair_witch.png");
-		}
-
-		[TestMethod]
-		public void OneboxYouTube()
-		{
-			var html = "<div class=\"onebox ob-youtube\"><a rel=\"nofollow noopener noreferrer\" style=\"text-decoration: none;\" href=\"https://www.youtube.com/watch?v=m3_I2rfApYk\"><img src=\"https://i2.ytimg.com/vi/m3_I2rfApYk/hqdefault.jpg\" width=\"240\" height=\"180\" class=\"ob-youtube-preview\"/><div class=\"ob-youtube-title\">Brexit, Briefly</div><div class=\"ob-youtube-overlay\">►</div></a></div>\r\n[Captain Obvious] botler, shutdown";
-			var text = html.ProcessStackMessage(RoomId, RoomName);
-			Assert.AreEqual(text, "https://www.youtube.com/watch?v=m3_I2rfApYk");
-		}
+		[TestCase("Hello <b>world</b>, how are <i>you?</i>", "Hello world, how are you?", TestName = "Message with multiple blocks")]
+		[TestCase("Foo &#xA9; bar", "Foo © bar", TestName = "Message html")]
+		[TestCase("<div class=\"full\">a <br> a</div>", "a \r\na", TestName = "Message multiline")]
+		[TestCase("<pre class=\"full\">@{\r\n    string email = item.Email;\r\n    if(item.isAdmin) email = $\"&lt;strong&gt;{email}&lt;/strong&gt;\r\n}\r\n@email</pre>", "```\r\n@{\r\n    string email = item.Email;\r\n    if(item.isAdmin) email = $\"<strong>{email}</strong>\r\n}\r\n@email\r\n```", TestName = "Onebox code")]
+		[TestCase("<div class=\"onebox ob-message\"><a rel=\"noopener noreferrer\" class=\"roomname\" href=\"/transcript/message/50954410#50954410\"><span title=\"2020-11-18 09:25:12Z\">46 mins ago</span></a>, by <span class=\"user-name\">d4rk4ng31</span> <br><div class=\"quote\">@Botler SHUT THE FUCK UP!</div></div>", "https://chat.stackoverflow.com/transcript/message/50954410#50954410", TestName = "Onebox message")]
+		[TestCase("<div class=\"onebox ob-image\"><a rel=\"nofollow noopener noreferrer\" href=\"https://i.redd.it/7apl34bga1061.png\"><img src=\"https://i.redd.it/7apl34bga1061.png\" class=\"user-image\" alt=\"user image\"></a></div>", "https://i.redd.it/7apl34bga1061.png", TestName = "Onebox image")]
+		[TestCase("<div class=\"onebox ob-xkcd\"><a rel=\"nofollow noopener noreferrer\" href=\"https://xkcd.com/2387\"><img src=\"https://imgs.xkcd.com/comics/blair_witch.png\" title=\"&quot;Are you concerned the witches won't breed in captivity?&quot; &quot;Honestly, we're more concerned that they WILL. We don't know what it involves, but our biologists theorize that it's 'harrowing.'&quot;\" alt=\"&quot;Are you concerned the witches won't breed in captivity?&quot; &quot;Honestly, we're more concerned that they WILL. We don't know what it involves, but our biologists theorize that it's 'harrowing.'&quot;\"></a></div>", "https://imgs.xkcd.com/comics/blair_witch.png", TestName = "Onebox XKCD")]
+		[TestCase("<div class=\"onebox ob-youtube\"><a rel=\"nofollow noopener noreferrer\" style=\"text-decoration: none;\" href=\"https://www.youtube.com/watch?v=m3_I2rfApYk\"><img src=\"https://i2.ytimg.com/vi/m3_I2rfApYk/hqdefault.jpg\" width=\"240\" height=\"180\" class=\"ob-youtube-preview\"/><div class=\"ob-youtube-title\">Brexit, Briefly</div><div class=\"ob-youtube-overlay\">►</div></a></div>\r\n[Captain Obvious] botler, shutdown", "https://www.youtube.com/watch?v=m3_I2rfApYk", TestName = "Onebox youtube")]
+		public void TestRawInputs(string html, string expected) => html.ProcessStackMessage(RoomId, RoomName).Should().Be(expected);
 	}
 }
